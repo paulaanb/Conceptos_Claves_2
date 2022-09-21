64e2c42494233f5761e20ca2b2d38241d4928897
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-09-21 16:48:59 +0200

    Definimos el cuadrante

--- codigo.py
+++ codigo.py
@@ -6,7 +6,22 @@ class Punto:
  
         self.coordenada_X = coordenada_X
         self.coordenada_Y = coordenada_Y
- def __str__(self):
+    def __str__(self):
         return  "({}, {})".format(self.coordenada_X, self.coordenada_Y)
- 
+#Definimos el cuadrante para poder trabajar con todos los ejes.
+    def cuadrante(self):
+        if self.coordenada_X > 0 and self.coordenada_Y > 0:
+            print("{} pertenece al primer cuadrante".format(self))
+        elif self.coordenada_X < 0 and self.coordenada_Y > 0:
+            print("{} pertenece al segundo cuadrante".format(self))
+        elif self.coordenada_X < 0 and self.coordenada_Y < 0:
+            print("{} pertenece al tercer cuadrante".format(self))
+        elif self.coordenada_X > 0 and self.coordenada_Y < 0:
+            print("{} pertenece al cuarto cuadrante".format(self))
+        elif self.coordenada_X != 0 and self.coordenada_Y == 0:
+            print("{} se sitúa sobre el eje X".format(self))
+        elif self.coordenada_X == 0 and self.coordenada_Y != 0:
+            print("{} se sitúa sobre el eje Y".format(self))
+        else:
+            print("{} se encuentra sobre el origen".format(self))
 
