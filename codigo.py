f4da9c29bc470ad59d8203453123652fcbee42d7
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-09-21 16:44:09 +0200

    Definimos la clase punto

---
+++ codigo.py
@@ -0,0 +1,7 @@
+#Definimos la clase punto
+class Punto:
+    def __init__(self, coordenada_X=0, coordenada_Y=0):
+ 
+        self.coordenada_X = coordenada_X
+        self.coordenada_Y = coordenada_Y
+ 
