6ff830d75719eacf11104e57d88d28873e996c7f
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-09-21 17:14:28 +0200

    importamos librerias

---
+++ unittesting.py
@@ -0,0 +1,8 @@
+#Importamos las librerias que podemos necesitar
+import inspect
+import re
+import unittest
+import math
+import pandas as pd
+import código as co
+from datetime import datetime, timedelta
